e27652fc614868119c0d188bc96f70b8a35fbc24
hammoumi123 <38195593+hammoumi123@users.noreply.github.com>
2023-03-06 14:28:44 +0000

    First_Commit
    First_Commit

---
+++ Controllers/DriversController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriversController : ControllerBase
+    {
+       
+
+        private readonly ILogger<DriversController> _logger;
+
+        public DriversController(ILogger<DriversController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Driver> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Driver
+            {
+              
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public void  Post(string jsonData)
+        {
+            //In Memory.
+            var driverInfo = JsonConvert.DeserializeObject<Driver>(jsonData);
+
+            Serilog.Log.Information(driverInfo.Driver_Fristname);
+            Serilog.Log.Information(driverInfo.Driver_Surname);
+            Serilog.Log.Information(driverInfo.age.ToString());
+
+
+
+            using (var streamWriter = new System.IO.StreamWriter(@"C:\ProjectFile\text.txt", true))
+            {
+                streamWriter.WriteLine(driverInfo.Driver_Fristname);
+                streamWriter.WriteLine(driverInfo.Driver_Surname);
+                streamWriter.WriteLine(driverInfo.age);
+            }
+         
+        }
+    }
+}
---
+++ Driver.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CapstoneProject
+{
+    public class Driver
+    {
+        public string Driver_Fristname { get; set; }
+
+        public string Driver_Surname { get; set; }
+
+        public int  age { get; set; }
+    }
+}
